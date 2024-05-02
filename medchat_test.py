02b01d9066396849cc404b03a3ca2282ab66fe77
Abdel-aziz Harane <122400488+AbdoulazizH@users.noreply.github.com>
2024-05-02 21:55:13 +0100

    Update medchat_test.py

--- medchat_test.py
+++ medchat_test.py
@@ -23,9 +23,9 @@ with st.sidebar:
     st.subheader('Modèles et paramètres') # Le choix de deux modèles LLama2
     selected_model = st.sidebar.selectbox('Choissisez un modèle Llama2', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
     if selected_model == 'Llama2-7B': #Premier modèle
-        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+        llm = 'a16z-infra/llama7b-v2-chat'
     elif selected_model == 'Llama2-13B': # second modèle
-        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+        llm = 'a16z-infra/llama13b-v2-chat'
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01) # Paramètres
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
     max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
